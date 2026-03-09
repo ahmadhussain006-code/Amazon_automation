d67065bddd65ce8a809307403646dabea20e487c
Ahmad Hussain <ahmad.hussain006@gmail.com>
2026-03-10 03:20:03 +0530

    code updated

---
+++ conftest.py
@@ -0,0 +1,7 @@
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chromium",
+        help="Browser: chromium, firefox, webkit"
+    )
--- test_login.py
+++ test_login.py
@@ -1,10 +1,23 @@
+import pytest
 from playwright.sync_api import sync_playwright
 
 
-def test_login():
+def test_login(request):
+
+    browser_name = request.config.getoption("browser")
+
     with sync_playwright() as p:
 
-        browser = p.chromium.launch(headless=True)
+        # Cross-browser launch
+        if browser_name == "chromium":
+            browser = p.chromium.launch(headless=False, args=["--start-maximized"])
+
+        elif browser_name == "firefox":
+            browser = p.firefox.launch(headless=True, args=["--start-maximized"])
+
+        else:
+            browser = p.webkit.launch(headless=True, args=["--start-maximized"])
+
         context = browser.new_context(no_viewport=True)
         page = context.new_page()
 
@@ -21,7 +34,7 @@ def test_login():
         page.wait_for_selector("//input[@type='email']", timeout=60000)
 
         # Enter email
-        page.fill("//input[@type='email']", "shizahasan63@gmail.com")
+        page.fill("//input[@type='email']", "test@email.com")
 
         # Click Continue
         page.click("//input[@type='submit']")
@@ -35,10 +48,10 @@ def test_login():
         # Click Sign In
         page.click("//input[@id='signInSubmit']")
 
-        # Wait a few seconds for redirect or login processing
+        # Wait for login process
         page.wait_for_timeout(8000)
 
-        # Print page title safely
+        # Print page title
         try:
             title = page.title()
             print("Page Title is:", title)
@@ -47,9 +60,4 @@ def test_login():
 
         print("Login test completed successfully.")
 
-        page.wait_for_timeout(5000)
         browser.close()
-
-
-if __name__ == "__main__":
-    test_login()
